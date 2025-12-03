5313efc0e013ad796c9f308932e678ede0725267
Totto16 <tobiausgais@gmail.com>
2025-12-03 07:59:49 +0100

    fix: solve example 1

--- src/days/day01/day.zig
+++ src/days/day01/day.zig
@@ -2,22 +2,73 @@ const std = @import("std");
 
 const utils = @import("utils");
 
+fn modHelper(input: i32, amount: u8) u8 {
+    return @intCast(@mod(input, amount));
+}
+
+const LineValue = struct { typ: u8, number: i32 };
+
+fn getLineValues(line: []const u8) ?LineValue {
+    if (line.len == 0) {
+        return null;
+    }
+
+    const typ = line[0];
+
+    const rest = line[1..];
+
+    const number = utils.parseInt(u32, rest, 10) catch {
+        return null;
+    };
+
+    return .{ .typ = typ, .number = @intCast(number) };
+}
+
 fn solve(allocator: utils.Allocator, input: utils.Str) utils.SolveResult {
     _ = allocator;
 
     var dial: u8 = 50;
+    var sum: u64 = 0;
 
-    var iter = utils.splitSeq(u8, input, "\n");
+    var iter = utils.splitSca(u8, input, '\n');
 
     while (iter.next()) |line| {
-        std.debug.print("here {s}\n", .{line});
-        dial += 1;
+        if (line.len == 0) {
+            continue;
+        }
+
+        const val = getLineValues(line);
+
+        if (val == null) {
+            return error.ParseError;
+        }
+
+        switch (val.?.typ) {
+            'L' => {
+                const dial_i32: i32 = dial;
+
+                dial = modHelper(dial_i32 - (val.?.number), 100);
+            },
+            'R' => {
+                const dial_i32: i32 = dial;
+
+                dial = modHelper(dial_i32 + (val.?.number), 100);
+            },
+            else => |c| {
+                std.debug.print("parse error: {c} was the start char", .{c});
+                return error.ParseError;
+            },
+        }
+
+        if (dial == 0) {
+            sum += 1;
+        }
     }
 
-    return error.NotSolved;
+    return .{ .u64 = sum };
 }
 
-const day = utils.Day{ .solver = utils.Solver{ .both = solve }, .examples = .{ .first = .{ .implemented = .{ .solution = .{ .u64 = 0 } } }, .second = .todo }, .root = @import("generated").root };
+const day = utils.Day{ .solver = utils.Solver{ .both = solve }, .examples = .{ .first = .{ .implemented = .{ .solution = .{ .u64 = 3 } } }, .second = .todo }, .root = @import("generated").root };
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
--- src/utils.zig
+++ src/utils.zig
@@ -131,13 +131,13 @@ pub const Day = struct {
         return result;
     }
 
-    fn printErrorString(str: []const u8) !void {
+    fn printErrorString(comptime fmt: []const u8) !void {
         var stderr_buffer: [1024]u8 = undefined;
         var stderr_writer = std.fs.File.stderr().writer(&stderr_buffer);
         const stderr = &stderr_writer.interface;
 
-        try ansi_term.format.updateStyle(stderr, ansi_term.style.Style{ .foreground = .Red });
-        stderr.print(str, .{});
+        try ansi_term.format.updateStyle(stderr, ansi_term.style.Style{ .foreground = .Red }, null);
+        try stderr.print(fmt, .{});
         try ansi_term.format.resetStyle(stderr);
         try stderr.flush();
     }
