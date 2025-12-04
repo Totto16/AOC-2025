51a519fe0cb26968988a44ea7b71cf659f6ffd7a
Totto16 <tobiausgais@gmail.com>
2025-12-04 07:36:54 +0100

    feat: solve day 04

--- src/days/day04/day.zig
+++ src/days/day04/day.zig
@@ -72,11 +72,7 @@ fn solveFirst(allocator: utils.Allocator, input: utils.Str) utils.SolveResult {
     defer rows.deinit();
 
     const tresh = 4;
-    var result: u54 = 0;
-
-    std.debug.print("\n", .{});
-    std.debug.print("\n", .{});
-    std.debug.print("\n", .{});
+    var result: u64 = 0;
 
     const position = [3]i32{ -1, 0, 1 };
 
@@ -129,17 +125,82 @@ fn solveFirst(allocator: utils.Allocator, input: utils.Str) utils.SolveResult {
 }
 
 fn solveSecond(allocator: utils.Allocator, input: utils.Str) utils.SolveResult {
-    _ = allocator;
-    _ = input;
+    var rows = try parseRows(allocator, input);
+    defer rows.deinit();
 
-    return utils.Solution{ .u64 = 0 };
+    const tresh = 4;
+    var total_result: u64 = 0;
+
+    outer: while (true) {
+        var result: u64 = 0;
+
+        const position = [3]i32{ -1, 0, 1 };
+
+        const height = rows.inner.items.len;
+
+        for (rows.inner.items, 0..) |row, y_u| {
+            const y_i: i32 = @intCast(y_u);
+            const width = row.inner.items.len;
+
+            for (row.inner.items, 0..) |cell, x_u| {
+                const x_i: i32 = @intCast(x_u);
+                if (cell != Paper.Paper) {
+                    continue;
+                }
+
+                var sum: u8 = 0;
+
+                for (position) |i| {
+                    for (position) |j| {
+                        if (i == 0 and j == 0) {
+                            continue;
+                        }
+
+                        const x: i32 = x_i + i;
+                        const y: i32 = y_i + j;
+
+                        if (x >= width or x < 0) {
+                            continue;
+                        }
+
+                        if (y >= height or y < 0) {
+                            continue;
+                        }
+
+                        const paper_at_pos: Paper = rows.inner.items[@intCast(y)].inner.items[@intCast(x)];
+
+                        if (paper_at_pos == Paper.Paper) {
+                            sum += 1;
+                        }
+                    }
+                }
+
+                if (sum < tresh) {
+                    result += 1;
+
+                    // we could store the positions and remove the papers later, but we can do it now, so everything termintes faster, as we might get to remove more, as this current one is also empty now!
+                    rows.inner.items[y_u].inner.items[x_u] = Paper.Empty;
+                }
+            }
+        }
+
+        if (result == 0) {
+            break :outer;
+        }
+
+        total_result += result;
+    }
+
+    return utils.Solution{ .u64 = total_result };
 }
 
 const generated = @import("generated");
 
 const day = utils.Day{
     .solver = utils.Solver{ .individual = .{ .first = solveFirst, .second = solveSecond } },
-    .examples = .{ .first = .{ .implemented = .{ .solution = .{ .u64 = 13 } } }, .second = .todo },
+    .examples = .{ .first = .{ .implemented = .{ .solution = .{ .u64 = 13 }, .real_value = .{ .u64 = 1549 } } }, .second = .{ .implemented = .{
+        .solution = .{ .u64 = 43 },
+    } } },
     .root = generated.root,
     .day = generated.day,
     .same_input = true,
