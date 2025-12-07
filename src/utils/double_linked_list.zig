4af5a3f1c5e15d22f0fb22b2f4326622f135a8f4
Totto16 <tobiausgais@gmail.com>
2025-12-07 16:02:48 +0100

    feat: use separate files for a stack and the double linked list

---
+++ src/utils/double_linked_list.zig
@@ -0,0 +1,131 @@
+const std = @import("std");
+
+pub fn DoublyLinkedList(comptime T: type) type {
+    return struct {
+        const Self = @This();
+
+        fn untyped_node_from_typed(typed_node: *Node) *InnerType.Node {
+            if (comptime @offsetOf(Node, "node") != 0) {
+                @compileError("Field `node` must be the first member of Node.");
+            }
+
+            return &typed_node.node;
+        }
+
+        fn typed_node_from_typed(typed_node: *InnerType.Node) *Node {
+            return @fieldParentPtr("node", typed_node);
+        }
+
+        pub const Node = struct {
+            node: std.DoublyLinkedList.Node,
+            value: T,
+
+            pub fn next(self: *Node) ?*Node {
+                const next_node = self.node.next;
+
+                if (next_node) |node| {
+                    return typed_node_from_typed(node);
+                }
+                return null;
+            }
+        };
+
+        pub const Iterator = struct {
+            current: ?*Node,
+
+            pub fn init(value: Self) Iterator {
+                return Iterator{ .current = value.first() };
+            }
+
+            pub fn next(self: *Iterator) ?*Node {
+                if (self.current) |node| {
+                    const next_node = node.next();
+                    if (next_node) |node2| {
+                        self.current = next_node;
+                        return node2;
+                    }
+                    return null;
+                }
+
+                return null;
+            }
+        };
+
+        const InnerType = std.DoublyLinkedList;
+
+        inner: InnerType,
+
+        pub fn init() Self {
+            return Self{
+                .inner = InnerType{
+                    .first = null,
+                    .last = null,
+                },
+            };
+        }
+
+        pub fn deinit(self: *Self) void {
+            self.inner.first = null;
+            self.inner.last = null;
+        }
+
+        pub fn first(self: *const Self) ?*Node {
+            const first_node = self.inner.first;
+            if (first_node) |node| {
+                return typed_node_from_typed(node);
+            }
+
+            return null;
+        }
+
+        pub fn last(self: *const Self) ?*Node {
+            const last_node = self.inner.last;
+            if (last_node) |node| {
+                return typed_node_from_typed(node);
+            }
+
+            return null;
+        }
+
+        pub fn append(self: *Self, new_node: *Node) void {
+            const node_ptr = untyped_node_from_typed(new_node);
+            self.inner.append(node_ptr);
+        }
+
+        pub fn pop(self: *Self) ?*Node {
+            const result = self.inner.pop();
+            if (result) |node| {
+                return typed_node_from_typed(node);
+            }
+
+            return null;
+        }
+
+        pub fn popFirst(self: *Self) ?*Node {
+            const result = self.inner.popFirst();
+            if (result) |node| {
+                return typed_node_from_typed(node);
+            }
+
+            return null;
+        }
+
+        pub fn prepend(self: *Self, new_node: *Node) void {
+            self.inner.prepend(new_node);
+        }
+
+        pub fn empty(self: *const Self) bool {
+            return self.inner.first == null;
+        }
+
+        pub fn iter(self: *Self) Iterator {
+            return Iterator{
+                .current = self.inner.first,
+            };
+        }
+
+        pub fn remove(self: *Self, node: *Node) void {
+            return self.inner.remove(untyped_node_from_typed(node));
+        }
+    };
+}
---
+++ src/utils/stack.zig
@@ -0,0 +1,87 @@
+const double_linked_list = @import("double_linked_list.zig");
+const std = @import("std");
+
+pub fn StackManaged(comptime T: type) type {
+    return struct {
+        const Self = @This();
+        pub const InnerType = double_linked_list.DoublyLinkedList(T);
+
+        inner: InnerType,
+        allocator: std.mem.Allocator,
+
+        pub const Slice = []T;
+        const Node = InnerType.Node;
+        const Iterator = InnerType.Iterator;
+
+        pub fn init(gpa: std.mem.Allocator) Self {
+            return Self{
+                .inner = InnerType.init(),
+                .allocator = gpa,
+            };
+        }
+
+        pub fn deinit(self: *Self) void {
+            var it: ?*InnerType.Node = self.inner.first();
+
+            while (it) |element| {
+                it = element.next();
+                self.allocator.destroy(element);
+            }
+
+            self.inner.deinit();
+        }
+
+        pub fn append(self: *Self, item: T) std.mem.Allocator.Error!void {
+            const node_ptr: *Node = try self.allocator.create(Node);
+            node_ptr.value = item;
+            self.inner.append(node_ptr);
+        }
+
+        pub fn prepend(self: *Self, item: T) std.mem.Allocator.Error!void {
+            const item_ptr: *T = try self.allocator.create(T);
+            item_ptr.* = item;
+            self.inner.prepend(item_ptr);
+        }
+
+        pub fn appendSlice(self: *Self, items: []const T) std.mem.Allocator.Error!void {
+            for (items) |item| {
+                try self.append(item);
+            }
+        }
+
+        pub fn popLast(self: *Self) ?T {
+            const result = self.inner.pop();
+
+            if (result) |node| {
+                const value = node.value;
+                self.allocator.destroy(node);
+                return value;
+            }
+            return null;
+        }
+
+        pub fn popFirst(self: *Self) ?T {
+            const result = self.inner.popFirst();
+
+            if (result) |node| {
+                const value = node.value;
+                self.allocator.destroy(node);
+                return value;
+            }
+            return null;
+        }
+
+        pub fn empty(self: *Self) bool {
+            return self.inner.empty();
+        }
+
+        pub fn iter(self: *Self) Iterator {
+            return Iterator.init(self.inner);
+        }
+
+        pub fn remove(self: *Self, node: *Node) void {
+            self.inner.remove(node);
+            self.allocator.destroy(node);
+        }
+    };
+}
